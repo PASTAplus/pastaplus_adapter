5dbc8a39f7a2867b2520685352a1033c8611b6a4
servilla <mark.servilla@gmail.com>
2017-03-06 17:18:54 -0700

    Remove class model; implement as main loop

--- src/parser.py
+++ src/parser.py
@@ -17,39 +17,35 @@ from datetime import datetime
 
 from lxml import etree
 
+from adapter_db import QueueManager
+from package import Package
+
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.INFO)
 
 logger = logging.getLogger('parser')
 
 
-class ChangeListParser(object):
-
-    def __init__(self, baseUrl='http://pasta.lternet.edu/package/changes/eml',
-                 fromDate=datetime.now().strftime(
-                     '%Y-%m-%dT%H:%M:%S.%f').rstrip('0')):
-        self.url = baseUrl + '?fromDate=' + fromDate
-        self.tree = etree.parse(self.url)
-
-    def list_packages(self):
-        for dataPackage in self.tree.getiterator('dataPackageUpload'):
-            packageId = dataPackage.find('./packageId')
-            print packageId.text
-            method = dataPackage.find('./serviceMethod')
-            print method.text
-            datetime = dataPackage.find('./date')
-            print datetime.text
-        #    for element in dataPackage:
-        #       if element.tag == 'packageId':
-        #          print element.text
-
-
 def main():
 
-    clp = ChangeListParser(baseUrl='http://pasta-s.lternet.edu/package'
-                                   '/changes/eml',
-                           fromDate='2017-02-01T00:00:00')
-    clp.list_packages()
+    qm = QueueManager()
+
+    baseUrl='http://pasta-s.lternet.edu/package/changes/eml'
+    fromDate = qm.get_last_datetime()
+    #if not fromDate:
+    fromDate='?fromDate=2017-02-01T00:00:00'
+
+    tree = etree.parse(baseUrl + fromDate)
+    for dataPackage in tree.getiterator('dataPackage'):
+        packageId = dataPackage.find('./packageId')
+        datetime = dataPackage.find('./date')
+        method = dataPackage.find('./serviceMethod')
+        logger.info('Enqueue: {p} - {d} - {m}'.format(p=packageId.text,
+                                                      d=datetime.text,
+                                                      m=method.text))
+        package = Package(package_str=packageId.text,
+                          datetime_str=datetime.text, method_str=method.text)
+        qm.enqueue(event_package=package)
 
     return 0
 
