49f0b3303da3417bd646659ae79befe428f186e3
servilla <mark.servilla@gmail.com>
2017-03-08 21:11:09 -0700

    Add logic to skip existing package event with starting fromDate

--- src/parser.py
+++ src/parser.py
@@ -18,18 +18,19 @@ from datetime import timedelta
 
 from lxml import etree
 
-from adapter_db import QueueManager
+from queue import QueueManager
 from package import Package
+import adapter_utilities
+import properties
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.INFO)
 
 logger = logging.getLogger('parser')
 
-BASE_URL = 'http://pasta-d.lternet.edu/package/changes/eml/?'
-
 
 def bootstrap():
+    url = properties.BASE_URL + '/changes/eml?'
     now = datetime.now()
     fromDate = datetime.strptime('2013-01-01T00:00:00.00',
                                  '%Y-%m-%dT%H:%M:%S.%f')
@@ -38,13 +39,12 @@ def bootstrap():
         fromDate_str = fromDate.strftime('%Y-%m-%dT%H:%M:%S.%f')
         toDate_str = toDate.strftime('%Y-%m-%dT%H:%M:%S.%f')
         logger.info('from: {f} -- to: {t}'.format(f=fromDate_str, t=toDate_str))
-        parse(baseUrl=BASE_URL, fromDate=fromDate_str, toDate=toDate_str)
+        parse(url=url, fromDate=fromDate_str, toDate=toDate_str)
         fromDate = toDate
 
 
-def parse(baseUrl=None, fromDate=None, toDate=None, scope=None):
+def parse(url=None, fromDate=None, toDate=None, scope=None):
 
-    url = baseUrl
     if fromDate:
         url = url + 'fromDate=' + fromDate + '&'
     if toDate:
@@ -58,23 +58,30 @@ def parse(baseUrl=None, fromDate=None, toDate=None, scope=None):
         packageId = dataPackage.find('./packageId')
         datetime = dataPackage.find('./date')
         method = dataPackage.find('./serviceMethod')
-        logger.info('Enqueue: {p} - {d} - {m}'.format(p=packageId.text,
-                                                      d=datetime.text,
-                                                      m=method.text))
-        package = Package(package_str=packageId.text,
-                          datetime_str=datetime.text, method_str=method.text)
-        qm.enqueue(event_package=package)
+        p = packageId.text
+        d = datetime.text
+        m = method.text
+
+        # Skip fromDate record(s) that already exist in queue
+        if fromDate == datetime.text:
+            logger.info('Skipping: {p} - {d} - {m}'.format(p=p, d=d, m=m))
+        else:
+            logger.info('Enqueue: {p} - {d} - {m}'.format(p=p, d=d, m=m))
+            package = Package(package_str=p, datetime_str=d,
+                              method_str=m)
+            qm.enqueue(event_package=package)
 
 
 def main():
 
+    url = properties.BASE_URL + '/changes/eml?'
     qm = QueueManager()
 
     fromDate = qm.get_last_datetime()
     if not fromDate:
         bootstrap()
     else:
-        parse(baseUrl=BASE_URL, fromDate=fromDate)
+        parse(url=url, fromDate=fromDate)
 
     return 0
 
