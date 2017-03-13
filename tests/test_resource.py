6a4b7beb143d6a1f99e81c61a455e6d4b0309ec5
servilla <mark.servilla@gmail.com>
2017-03-13 16:22:20 -0600

    Remove explicit URLs in favor of properties.PASTA_BASE_URL and test for build_system_metadata

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -15,6 +15,10 @@
 import unittest
 import logging
 
+import properties
+from resource import Resource
+
+
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.WARN)
 
@@ -22,11 +26,19 @@ logger = logging.getLogger('test_resource')
 
 
 class TestResource(unittest.TestCase):
+
+    metadata_resource = properties.PASTA_BASE_URL + 'metadata/eml/knb-lter-nin/1/1'
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
+
+    def test_build_system_metadata(self):
+        resource = Resource(resource=TestResource.metadata_resource)
+        sm = resource._build_system_metadata()
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
