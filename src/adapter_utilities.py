aef782c32845f2f56bbfc16292a6f1245ae39ec6
servilla <mark.servilla@gmail.com>
2017-03-13 21:02:21 -0600

    Add stale formats file check

--- src/adapter_utilities.py
+++ src/adapter_utilities.py
@@ -14,6 +14,9 @@
 
 import logging
 import pickle
+import os.path
+from datetime import datetime
+from datetime import timedelta
 
 from d1_client.cnclient_2_0 import CoordinatingNodeClient_2_0
 
@@ -28,33 +31,53 @@ logger = logging.getLogger('adapter_utilities')
 def make_https(url=None):
     return url.replace('http:', 'https:', 1)
 
+
 def make_http(url=None):
     return url.replace('https:', 'http:', 1)
 
 
 def get_d1_formats(url=properties.D1_BASE_URL):
-    """Return dict of D1 formats as pyxb format object
+    """Return dict of D1 formats as pyxb format object.
 
     Cache formats in "formats.pkl" pickle if D1 listformats()
-    fails.
+    fails or if formats is stale based on file modify time.
 
     :param url: D1 base url
     :return: dict of D1 formats as pyxb format object
     """
+    formats_file = '../cache/formats.pkl'
     formats = {}
+    if _is_stale_file(filename=formats_file, seconds=3600):
+        try:
+            cn = CoordinatingNodeClient_2_0(base_url=url)
+            format_list = cn.listFormats()
+            for _format in format_list.objectFormat:
+                formats[_format.formatId] = _format
+            fp = open(formats_file, 'wb')
+            pickle.dump(formats, file=fp)
+        except Exception as e:
+            logger.error(e)
     try:
-        cn = CoordinatingNodeClient_2_0(base_url=url)
-        format_list = cn.listFormats()
-        for format in format_list.objectFormat:
-            formats[format.formatId] = format
-        fp = open('formats.pkl', 'wb')
-        pickle.dump(formats, file=fp)
-    except Exception as e:
-        logger.error(e)
-        fp = open('formats.pkl', 'rb')
+        fp = open(formats_file, 'rb')
         formats = pickle.load(file=fp)
-
+    except IOError as e:
+        logger.error(e)
     return formats
+
+
+def _is_stale_file(filename=None, seconds=None):
+    is_stale = False
+    try:
+        mtime = os.path.getmtime(filename=filename)
+        delta = datetime.fromtimestamp(timestamp=mtime) + timedelta(seconds=seconds)
+        if delta < datetime.now():
+            is_stale = True
+    except OSError as e:
+        logger.error(e)
+        is_stale = True
+    return is_stale
+
+
 
 def main():
 
