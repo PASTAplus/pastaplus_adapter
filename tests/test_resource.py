1608e7f95ccb56b937cf95bfa7f9cabf27b44aea
servilla <mark.servilla@gmail.com>
2017-03-15 21:33:30 -0600

    Add logging basic configuration and principal owner for build system metadata test

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 """:Mod: test_resource
 
 :Synopsis:
@@ -13,6 +15,10 @@
 """
 
 import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.INFO)
+
 import unittest
 
 from datetime import datetime
@@ -21,13 +27,12 @@ import properties
 from resource import Resource
 from package import Package
 
-
 logger = logging.getLogger('test_resource')
 
 
 class TestResource(unittest.TestCase):
-
-    metadata_resource = properties.PASTA_BASE_URL + 'metadata/eml/knb-lter-nin/1/1'
+    metadata_resource = properties.PASTA_BASE_URL + \
+                        'metadata/eml/knb-lter-nin/1/1'
     package_str = 'knb-lter-nin.1.1'
     scope = 'knb-lter-nin'
     identifier = 1
@@ -35,14 +40,15 @@ class TestResource(unittest.TestCase):
     datetime_str = '2017-02-23T13:09:29.166'
     datetime = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%f')
     method_str = 'createDataPackage'
+    owner_str = 'uid=LNO,o=LTER,dc=ecoinformatics,dc=org'
     doi = 'doi:10.6073/pasta/3bcc89b2d1a410b7a2c678e3c55055e1'
     url = properties.PASTA_BASE_URL
-
 
     def setUp(self):
         self.package = Package(package_str=TestResource.package_str,
                                datetime_str=TestResource.datetime_str,
-                               method_str=TestResource.method_str)
+                               method_str=TestResource.method_str,
+                               owner_str=TestResource.owner_str)
 
     def tearDown(self):
         pass
@@ -51,7 +57,9 @@ class TestResource(unittest.TestCase):
         resources = self.package.get_resources()
         for resource in resources:
             resource = Resource(resource=resource)
-            sm = resource._build_system_metadata()
+            sm = resource.get_system_metadata(
+                rights_holder=self.package.get_owner())
+            print(sm)
         self.assertTrue(True)
 
 
