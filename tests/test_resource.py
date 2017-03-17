59303725cd97ac13b9cb736da579f4b5198b28c5
servilla <mark.servilla@gmail.com>
2017-03-16 21:11:25 -0600

    Minor formatting change

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -20,7 +20,6 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.INFO)
 
 import unittest
-
 from datetime import datetime
 
 import properties
