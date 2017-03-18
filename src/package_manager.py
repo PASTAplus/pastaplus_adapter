adb4329ebdc15cfe45f92538c70c0492004e4181
servilla <mark.servilla@gmail.com>
2017-03-17 22:05:09 -0600

    Add D1 client connections

--- src/package_manager.py
+++ src/package_manager.py
@@ -15,12 +15,16 @@ from __future__ import print_function
 """
 
 import logging
+import StringIO
+
+import d1_common
+import d1_client.cnclient_2_0
+import d1_client.mnclient_2_0
+import d1_client.data_package
 
 from queue import QueueManager
-from package import Package
-import adapter_utilities
+from resource import Resource
 import properties
-
 
 logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S%z', level=logging.INFO)
@@ -28,28 +32,59 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 logger = logging.getLogger('package_manager')
 
 
+def _make_metadata_url(package_path=None):
+    return properties.PASTA_BASE_URL + 'metadata/eml/' + package_path
+
+
+def _make_package_url(package_path=None):
+    return properties.PASTA_BASE_URL + 'eml/' + package_path
+
+
+def create_gmn_client(self):
+    return d1_client.mnclient_2_0.MemberNodeClient_2_0(
+        base_url=properties.GMN_BASE_URL,
+        cert_path=properties.GMN_CERT_PATH,
+        key_path=properties.GMN_PRIVATE_KEY_PATH,
+        timeout=properties.GMN_RESPONSE_TIMEOUT,
+    )
+
+
 def main():
     qm = QueueManager()
-    url = properties.PASTA_BASE_URL
-    formats = adapter_utilities.get_d1_formats()
-
     package = qm.get_head()
     while package:
         if package.is_public():
             logger.info('Processing: {p}'.format(p=package.get_package_str()))
+            gmn_client = create_gmn_client()
+
+            predecessor = qm.get_predecessor(event_package=package)
+            while not predecessor.is_public():
+                predecessor = qm.get_predecessor(event_package=predecessor)
+
             resources = package.get_resources()
-            logger.info(resources)
-            # for each resource:
-                # Build system metadata object
-            # Build ORE using DOI
-            # Determine if create or update
-            # lineage = [package.get_package_str()]
-            # predecessor = qm.get_predecessor(event_package=package)
-            # while predecessor:
-            #     lineage.append(predecessor.get_package_str())
-            #     predecessor = qm.get_predecessor(event_package=predecessor)
-            # print(lineage)
-            # Push to GMN
+            for resource in resources:
+                r = Resource(resource)
+                r_sys_meta = r.get_d1_system_metadata(
+                    rights_holder=package.get_owner())
+                vse_header = r.get_vendor_specific_ext_header()
+                if predecessor and r.get_type() == properties.METADATA:
+                    old_pid = _make_metadata_url(predecessor.package_path)
+                    gmn_client.update(pid=old_pid, obj=StringIO.StringIO(),
+                                      newPid=resource, sysmeta=r_sys_meta,
+                                      vendorSpecific=vse_header)
+                else:
+                    gmn_client.create(pid=resource, obj=StringIO.StringIO(),
+                                      sysmeta=r_sys_meta,
+                                      vendorSpecific=vse_header)
+                # Build ORE using DOI
+                    # Determine if create or update
+                    # lineage = [package.get_package_str()]
+                    # predecessor = qm.get_predecessor(event_package=package)
+                    # while predecessor:
+                    #     lineage.append(predecessor.get_package_str())
+                    #     predecessor = qm.get_predecessor(event_package=predecessor)
+                    # print(lineage)
+                    # Push to GMN
         qm.dequeue(event_package=package)
         package = qm.get_head()
 
