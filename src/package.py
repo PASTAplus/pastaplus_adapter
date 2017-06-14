5f85e2e0b99f2be87a746820bea5bcf28253e669
servilla <mark.servilla@gmail.com>
2017-06-14 13:40:11 -0600

    Reorder methods alpha; make class/init vars private

--- src/package.py
+++ src/package.py
@@ -28,11 +28,11 @@ class Package(object):
     def __init__(self, package_str=None, datetime_str=None, method_str=None,
                  owner_str=None, doi_str=None):
         self._package_str = package_str.strip()
-        self.package = self.package_str.split(".")
-        self._scope = self.package[0]
-        self._identifier = int(self.package[1])
-        self._revision = int(self.package[2])
-        self.package_path = self.package_str.replace('.', '/')
+        self._package = self.package_str.split(".")
+        self._scope = self._package[0]
+        self._identifier = int(self._package[1])
+        self._revision = int(self._package[2])
+        self._package_path = self.package_str.replace('.', '/')
         self._resources_dict = self._get_dict_of_resources()
 
         if 'T' in datetime_str:
@@ -49,46 +49,6 @@ class Package(object):
             self._doi = self.package_purl
         else:
             self._doi = doi_str.strip()
-
-    @property
-    def package_str(self):
-        return self._package_str
-
-    @property
-    def package_purl(self):
-        return properties.PASTA_BASE_URL + 'eml/' + self.package_path
-
-    @property
-    def scope(self):
-        return self._scope
-
-    @property
-    def identifier(self):
-        return self._identifier
-
-    @property
-    def revision(self):
-        return self._revision
-
-    @property
-    def resources(self):
-        return self._resources_dict
-
-    @property
-    def datetime(self):
-        return self._datetime
-
-    @property
-    def method(self):
-        return self._method
-
-    @property
-    def owner(self):
-        return self._owner
-
-    @property
-    def doi(self):
-        return self._doi
 
     def _get_dict_of_resources(self):
         """
@@ -123,19 +83,57 @@ class Package(object):
 
         return resources_dict
 
-    def is_public(self):
+    @property
+    def datetime(self):
+        return self._datetime
 
+    @property
+    def doi(self):
+        return self._doi
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @property
+    def package_purl(self):
+        return properties.PASTA_BASE_URL + 'eml/' + self._package_path
+
+    @property
+    def package_str(self):
+        return self._package_str
+
+    @property
+    def public(self):
         resource = self._resources_dict[properties.METADATA]
         r = Resource(resource=resource)
         if not r.is_public():
             return False
-
         for resource in self._resources_dict[properties.DATA]:
             r = Resource(resource=resource)
             if not r.is_public():
                 return False
-
         return True
+
+    @property
+    def resources(self):
+        return self._resources_dict
+
+    @property
+    def revision(self):
+        return self._revision
+
+    @property
+    def scope(self):
+        return self._scope
 
 
 def main():
