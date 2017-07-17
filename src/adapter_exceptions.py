bebe053ab85df0fbd0504ccc025ef3519c4a7f93
servilla <mark.servilla@gmail.com>
2017-07-17 16:40:46 -0600

    Add AdapterUnknownResourceTypeException

--- src/adapter_exceptions.py
+++ src/adapter_exceptions.py
@@ -28,3 +28,11 @@ class AdapterUrlException(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class AdapterUnknownResourceTypeException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
